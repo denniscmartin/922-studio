5b1bb07921fd5d8283f62ee9b6ad7f6cfb21cd0e
Dennis <dennisconcepcionmartin@gmail.com>
2022-12-24 11:49:48 +0000

    update project view

--- web/models.py
+++ web/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
-# Create your models here.
 
 class Project(models.Model):
     name = models.CharField(max_length=80)
+    category = models.CharField(max_length=20)
+    created_date = models.DateTimeField(auto_now_add=True)
     url = models.URLField()
-    image = models.URLField()
+    image_url = models.URLField()
 
     def __str__(self):
         return self.name
--- web/views.py
+++ web/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import Project
+
 
 def index(request):
-    return render(request, 'index.html')
+    projects = Project.objects.order_by('-created_date')
+    context = {'projects': projects}
+
+    return render(request, 'index.html', context)
