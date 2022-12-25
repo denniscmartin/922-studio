4d76a852caa2a1e8518bae473c1e70f21c308096
Dennis <dennisconcepcionmartin@gmail.com>
2022-12-25 11:35:50 +0000

    add localization

--- web/views.py
+++ web/views.py
@@ -1,9 +1,20 @@
+import json
+
 from django.shortcuts import render
+from django.conf import settings
+from django.http import Http404
+
 from .models import Project
 
 
-def index(request):
+def index(request, lang='es'):    
+    if f'{lang.lower()}.json' not in settings.SUPPORTED_LOCALES:
+        raise Http404("Localization not found")
+
+    with open(f'{settings.STATIC_ROOT}/localization/{lang}.json') as f:
+        context = json.load(f)
+
     projects = Project.objects.order_by('-created_date')
-    context = {'projects': projects}
+    context['projects'] = projects
 
     return render(request, 'index.html', context)
